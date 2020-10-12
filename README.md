5580784f8ff3246b3c27356bd24ca36487cfc78d
sidninja210 <ninja.siddhartha@gmail.com>
2020-10-12 10:50:34 -0700

    minor changes

--- README.md
+++ README.md
@@ -5,3 +5,15 @@
 [http://bingo-api-env.eba-zpgsctry.us-west-1.elasticbeanstalk.com/](http://bingo-api-env.eba-zpgsctry.us-west-1.elasticbeanstalk.com/)
 
 ## Requests:
+### get /games
+returns list of all games
+### get /games/:key
+returns the game with the specified key param
+### post /join/:key?name=...&id=...
+admits the player into the game
+### post /chat/:key?from=...&body=...
+sends a chat message
+
+
+## Socket.io events:
+game${key}-updated
--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const cors = require("cors");
 const http = require("http");
+const { throws } = require("assert");
 
 
 const app = express();
@@ -10,9 +11,14 @@ app.get("/", (req, res) => {
     res.send("BINGO API by Siddhartha Chatterjee")
 });
 
+
 const server = http.createServer(app);
 server.listen(process.env.PORT || 8080);
 const socketIO = require("socket.io")(server);
+
+const emitUpdate = room => socketIO.emit(`game${room}-updated`);
+
+const TICKET_COST = 2, STARTING_MONEY = 5;
 
 const games = {};
 
@@ -20,9 +26,7 @@ function generateTicket() {
     const ticket = Array(3).fill(null).map(() => Array(9).fill(null));
     const availableNumbers = Array(90).fill(null).map((_, i) => i + 1);
     for (let i = 0; i < 3; ++i) {
-       // const availableInRow = availableNumbers;
         for (let j = 0; j < 5; ++j) {
-            //let randi;
             do {
                 var randi = Math.floor(Math.random() * availableNumbers.length);
             } while (ticket[i].some(num => Math.floor(num / 10) == Math.floor(availableNumbers[randi] / 10)))
@@ -33,4 +37,45 @@ function generateTicket() {
     return ticket;
 }
 
-console.log(generateTicket());
+function generateKey() {
+    do {
+        var key = Math.floor(Math.random() * 1e5 * 9) + 1e5;
+    } while (key.toString() in games)
+    return key;
+}
+
+class Player {
+    tickets = [];
+    money = STARTING_MONEY;
+    name;
+    id;
+    constructor(name, id) {
+        this.id = id;
+        this.name = name;
+    }
+    buyTicket() {
+        if (this.money > TICKET_COST) {
+            this.tickets.push(generateTicket());
+            this.money -= TICKET_COST;
+        }
+    }
+}
+
+class Game {
+    key;
+    players = [];
+    host;
+    chat = [];
+    constructor(host, hostid) {
+        this.key = generateKey();
+        this.host = host;
+        this.players.push(new Player(host, hostid));
+    }
+    join(playername, playerid) {
+        this.players.push(new Player(playername, playerid))
+    }
+}
+
+app.get("/games", (req, res) => {
+    res.send(JSON.stringify(games))
+})
