d24abf9c198b820a6833f9bd0f17e0318bcc5ec8
sidninja210 <ninja.siddhartha@gmail.com>
2020-10-12 10:06:21 -0700

    socket.io connection added

--- app.js
+++ app.js
@@ -2,12 +2,14 @@ const express = require("express");
 const cors = require("cors");
 const http = require("http");
 
+
 const app = express();
 app.use(cors());
 
 app.get("/", (req, res) => {
     res.send("BINGO API by Siddhartha Chatterjee")
-})
+});
 
 const server = http.createServer(app);
 server.listen(process.env.PORT || 8080);
+const socketIO = require("socket.io")(server);
